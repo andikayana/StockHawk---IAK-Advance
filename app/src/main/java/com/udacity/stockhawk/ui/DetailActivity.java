09ebf48033a8e4e776cb86a95d8e5ae3897d266d
andika yana <andikayana99@gmail.com>
2017-12-10 01:05:35 +0800

    change color + add back in detail + add menu share

--- app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.udacity.stockhawk.R;
 
@@ -16,6 +22,8 @@ public class DetailActivity extends AppCompatActivity {
 
     @BindView(R.id.webview)
     WebView mWebView;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,8 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         ButterKnife.bind(this);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        progressBar.setVisibility(View.VISIBLE);
 
         String[] stock = getIntent().getStringArrayExtra(Intent.EXTRA_TEXT);
         setTitle(stock[1]);
@@ -33,7 +43,49 @@ public class DetailActivity extends AppCompatActivity {
                 .authority("empyrean-aurora-455.appspot.com")
                 .appendPath("charts.php")
                 .appendQueryParameter("symbol", stock[0]);
-        String url = builder.build().toString();
+        final String url = builder.build().toString();
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
         mWebView.loadUrl(url);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            //tombol Arrow for back
+            case android.R.id.home:
+                DetailActivity.this.finish();
+                return true;
+            //tombl share
+            case R.id.menu_share:
+                String[] stock = getIntent().getStringArrayExtra(Intent.EXTRA_TEXT);
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareSubject = stock[1];
+                String shareText="http://empyrean-aurora-455.appspot.com/charts.php?symbol=" + stock[0];
+                sharingIntent.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+                startActivity(Intent.createChooser(sharingIntent, "Share Link Using"));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
